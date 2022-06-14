f0b098a51694bb89ffb295071a1940e01cdc42dd
febryntara <paglutgede@gmail.com>
2022-06-14 21:45:20 +0800

    protofolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#222537',
+        'primary-lighten': '#393E5C',
+        'secondary': '#14B524',
+        'secondary-darken': '#12901F',
+        'tertiary': '#fff',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+      }
+    },
+  },
+  plugins: [],
+}
